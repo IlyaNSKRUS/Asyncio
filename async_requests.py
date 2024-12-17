275dd8f2e5c3f32ef851071143c50ec267016dab
PC Ubuntu <glyanenkoiv@gmail.com>
2024-12-17 19:47:41 +0700

    correction_v2

--- async_requests.py
+++ async_requests.py
@@ -10,37 +10,27 @@ MAX_COROS = 5
 
 async def get_people_info(people_id, session):
     response = await session.get(f'https://swapi.py4e.com/api/people/{people_id}/')
-    json_data = await response.json()
-    films_list = []
-    species_list = []
-    starships_list = []
-    vehicles_list = []
-    for item in json_data:
-        if 'name' in item:
-            for i in json_data['films']:
-                response = await session.get(i)
-                json_data_film = await response.json()
-                json_data['films'] = json_data_film['title']
-                films_list.append(json_data['films'])
-            json_data['films'] = films_list
-            for i in json_data['species']:
-                response = await session.get(i)
-                json_data_species = await response.json()
-                json_data['species'] = json_data_species['name']
-                species_list.append(json_data['species'])
-            json_data['species'] = species_list
-            for i in json_data['starships']:
-                response = await session.get(i)
-                json_data_starships = await response.json()
-                json_data['starships'] = json_data_starships['name']
-                starships_list.append(json_data['starships'])
-            json_data['starships'] = starships_list
-            for i in json_data['vehicles']:
-                response = await session.get(i)
-                json_data_vehicles = await response.json()
-                json_data['vehicles'] = json_data_vehicles['name']
-                vehicles_list.append(json_data['vehicles'])
-            json_data['vehicles'] = vehicles_list
+    # print(response.status)
+    if response.status == 200:
+        json_data = await response.json()
+        # print(json_data['url'])
+        list = ['films', 'species', 'starships', 'vehicles']
+        for i in list:
+            json_data[i] = await get_people_description(json_data[i], session)
+            # print(f'json_data["i"] = {json_data[i]}')
+        return json_data
+
+
+async def get_people_description(links, session):
+    json_data_temp = []
+    for i in links:
+        response = await session.get(i)
+        data = await response.json()
+        if 'name' in data:
+            json_data_temp.append(data['name'])
+        else:
+            json_data_temp.append(data['title'])
+    json_data = ', '.join(json_data_temp)
     return json_data
 
 
@@ -48,7 +38,7 @@ async def insert_to_database(list_json: list[dict]):
     async with Session() as session:
         for item in list_json:
             objects = []
-            if 'name' in item:
+            if item is not None:
                 objects = [SwapiPeople(name=item['name'],
                                        height=item['height'],
                                        mass=item['mass'],
